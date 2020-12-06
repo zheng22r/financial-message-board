f625b56faf545e53257d721cd384cc6ce74a973f
zheng22r <andyzheng1994@gmail.com>
2020-12-05 20:22:00 -0600

    seperate frontend and backend

--- src/app/stock/pipes/helpers.ts
+++ src/app/stock/pipes/helpers.ts
@@ -1,5 +1,4 @@
-import { getDate, addDays, MS_PER_DAY } from "@progress/kendo-date-math";
-import { SelectionRange } from "@progress/kendo-angular-dateinputs";
+import { MS_PER_DAY } from "@progress/kendo-date-math";
 
 export const formatCurrency = (value: number): any => {
     if (value >= 1000000000) {
@@ -15,40 +14,6 @@ export const formatCurrency = (value: number): any => {
     }
 
     return value;
-};
-
-export const dateInRange = (candidate: Date, min: Date, max: Date): Date => {
-    if (!candidate) {
-        return candidate;
-    }
-
-    if (min && candidate < min) {
-        return min;
-    }
-
-    if (max && candidate > max) {
-        return max;
-    }
-
-    return candidate;
-};
-
-export const isDateInRange = (candidate: Date, min: Date, max: Date): boolean => (
-    !candidate || !((min && min > candidate) || (max && max < candidate))
-);
-
-export const normalizeSelectionRange = (start: Date, end: Date, min: Date, max: Date): SelectionRange => {
-    if (!(start && end && isDateInRange(start, min, max) && isDateInRange(end, min, max))) {
-        return { start: null, end: null };
-    }
-
-    const normalizedStart = getDate(start);
-    const normalizedEnd = addDays(end, 1);
-
-    return {
-        start: dateInRange(normalizedStart, min, max),
-        end: dateInRange(normalizedEnd, min, max)
-    };
 };
 
 export const rangeAndIntervalCompatible = (rangeDuration: number, intervalDuration: number) => {
