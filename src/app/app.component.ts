1dff958e3b445c68b8571647ba67bc886c1536ec
Nityang Patel <pate1219@algonquinlive.com>
2024-01-07 19:14:56 -0500

    Hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from "./auth/auth.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
+  // testing testing
   constructor(
     private authService: AuthService,
   ) {}
